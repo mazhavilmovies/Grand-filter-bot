211d2fd00071031136c8aa74166d7df749c47be7
mazhavilmovies <m2185351@gmail.com>
2025-11-29 14:31:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,7 +28,7 @@ MELCOW_VID = environ.get("MELCOW_VID", "https://graph.org/file/ea40f1b53dd3b6315
 SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/e8d6c2a82bb138cec2aef-3b0eee26cb8aafe98b.jpg")
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1933114137 6970728256').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1933114137 5020503671').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002535364932').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '-1002740788204 -1002591168134').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
